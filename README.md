21a7e2bfb91f166dffe96138a3fd55b33fc04958
LeDevNovice <gregory.saison@proton.me>
2025-08-09 12:40:26 +0200

    chore(scaffold): init project architecture

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# mini-tq
+# Mini TQ
+
+*This repo reproduce progressively internal bricks of Tanstack Query (v5) to understand **why** and **how** the lib runs internally.
+
+
---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        include: ['packages/**/tests/**/*.test.ts'],
+        environment: 'node',
+        globals: true,
+        restoreMocks: true,
+        clearMocks: true,
+        unstubGlobals: true,
+        unstubEnvs: true
+    }
+});
